877abbef78de696df9ad743d9cd96688e403c9b6
Michail Bredichin <michailbredichin@gmail.com>
2025-12-01 15:58:58 +0100

    tailwind, skeleton and lucide react

---
+++ quizzznite/src/app/challenges/page.js
@@ -0,0 +1,73 @@
+import { User } from 'lucide-react'
+export default function Challenge ()  {
+    return (
+    
+        <div class="space-y-4">
+                <User name="home" />
+	<div class="flex gap-4">
+		<button type="button" class="btn preset-filled-primary-500">Button</button>
+		<button type="button" class="btn preset-tonal-primary">Button</button>
+		<button type="button" class="btn preset-outlined-primary-500">Button</button>
+	</div>
+	<div class="flex gap-4">
+		<button type="button" class="btn preset-filled-secondary-500">Button</button>
+		<button type="button" class="btn preset-tonal-secondary">Button</button>
+		<button type="button" class="btn preset-outlined-secondary-500">Button</button>
+	</div>
+	<div class="flex gap-4">
+		<button type="button" class="btn preset-filled-tertiary-500">Button</button>
+		<button type="button" class="btn preset-tonal-tertiary">Button</button>
+		<button type="button" class="btn preset-outlined-tertiary-500">Button</button>
+	</div>
+	<div class="flex gap-4">
+		<button type="button" class="btn preset-filled-success-500">Button</button>
+		<button type="button" class="btn preset-tonal-success">Button</button>
+		<button type="button" class="btn preset-outlined-success-500">Button</button>
+	</div>
+	<div class="flex gap-4">
+		<button type="button" class="btn preset-filled-warning-500">Button</button>
+		<button type="button" class="btn preset-tonal-warning">Button</button>
+		<button type="button" class="btn preset-outlined-warning-500">Button</button>
+	</div>
+	<div class="flex gap-4">
+		<button type="button" class="btn preset-filled-error-500">Button</button>
+		<button type="button" class="btn preset-tonal-error">Button</button>
+		<button type="button" class="btn preset-outlined-error-500">Button</button>
+	</div>
+	<div class="flex gap-4">
+		<button type="button" class="btn preset-filled-surface-500">Button</button>
+		<button type="button" class="btn preset-tonal-surface">Button</button>
+		<button type="button" class="btn preset-outlined-surface-500">Button</button>
+	</div>
+    <div class="w-full grid grid-cols-3 gap-4">
+	<div class="card p-4 preset-filled-primary-500">Card</div>
+	<div class="card p-4 preset-tonal-primary">Card</div>
+	<div class="card p-4 preset-outlined-primary-500">Card</div>
+
+	<div class="card p-4 preset-filled-secondary-500">Card</div>
+	<div class="card p-4 preset-tonal-secondary">Card</div>
+	<div class="card p-4 preset-outlined-secondary-500">Card</div>
+
+	<div class="card p-4 preset-filled-tertiary-500">Card</div>
+	<div class="card p-4 preset-tonal-tertiary">Card</div>
+	<div class="card p-4 preset-outlined-tertiary-500">Card</div>
+
+	<div class="card p-4 preset-filled-success-500">Card</div>
+	<div class="card p-4 preset-tonal-success">Card</div>
+	<div class="card p-4 preset-outlined-success-500">Card</div>
+
+	<div class="card p-4 preset-filled-warning-500">Card</div>
+	<div class="card p-4 preset-tonal-warning">Card</div>
+	<div class="card p-4 preset-outlined-warning-500">Card</div>
+
+	<div class="card p-4 preset-filled-error-500">Card</div>
+	<div class="card p-4 preset-tonal-error">Card</div>
+	<div class="card p-4 preset-outlined-error-500">Card</div>
+
+	<div class="card p-4 preset-filled-surface-500">Card</div>
+	<div class="card p-4 preset-tonal-surface">Card</div>
+	<div class="card p-4 preset-outlined-surface-500">Card</div>
+</div>
+</div>
+    )
+}
--- quizzznite/src/app/layout.js
+++ quizzznite/src/app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="cerberus">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
